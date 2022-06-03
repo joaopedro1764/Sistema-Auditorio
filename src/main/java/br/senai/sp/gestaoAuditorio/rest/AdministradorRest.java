abd47205e1498103440ddbd6a31c6ccd4f1c7837
Romeu <passadore.neto@outlook.com>
2022-06-02 21:02:16 -0300

    admin

--- src/main/java/br/senai/sp/gestaoAuditorio/rest/AdministradorRest.java
+++ src/main/java/br/senai/sp/gestaoAuditorio/rest/AdministradorRest.java
@@ -1,11 +1,15 @@
 package br.senai.sp.gestaoAuditorio.rest;
 
 import java.net.URI;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import br.senai.sp.gestaoAuditorio.annotation.Publico;
 import br.senai.sp.gestaoAuditorio.model.Administrador;
+import br.senai.sp.gestaoAuditorio.model.TokenJWT;
 import br.senai.sp.gestaoAuditorio.repository.AdministradorRepository;
 
 @RestController
@@ -23,6 +31,9 @@ public class AdministradorRest {
 
 	@Autowired
 	private AdministradorRepository repository;
+	// constantes para gerar o token
+	public static final String EMISSOR = "r@meu";
+	public static final String SECRET = "RES@PLICA";
 
 	@Publico
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -55,5 +66,37 @@ public class AdministradorRest {
 	public Iterable<Administrador> getAllValues() {
 		return repository.findAll();
 	}
-
+	@Publico
+	@RequestMapping(value = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<TokenJWT> logar(@RequestBody Administrador administrador) {
+		System.out.println(administrador.getNif());
+		System.out.println(administrador.getSenha());
+		// busca o usuário no BD
+		administrador = repository.findByNifAndSenha(administrador.getNif(), administrador.getSenha());
+		// verifica se existe o usuário
+		if (administrador != null) {
+			System.out.println("Entrou aqui !");
+			// valores adicionais para o token
+			Map<String, Object> payload2 = new HashMap<String, Object>();
+			payload2.put("id_admin", administrador.getId());
+			payload2.put("nome_admin", administrador.getNome());
+			// definir a data de expiração
+			Calendar expiracao = Calendar.getInstance();
+			expiracao.add(Calendar.HOUR, 1);
+			// algoritmo para assinar o token
+			Algorithm algoritmo2 = Algorithm.HMAC256(SECRET);
+			// gerar o token
+			TokenJWT tokenJwt2 = new TokenJWT();
+			tokenJwt2.setToken(JWT.create().withPayload(payload2).withIssuer(EMISSOR).withExpiresAt(expiracao.getTime())
+					.sign(algoritmo2));
+			
+			System.out.println(tokenJwt2);
+			return ResponseEntity.ok(tokenJwt2);
+			
+		} else {
+			return new ResponseEntity<TokenJWT>(HttpStatus.UNAUTHORIZED);
+		}
+	}
 }
+
+
---
+++ src/main/resources/static/admin.js
@@ -0,0 +1,76 @@
+
+//function colocaValor() {
+//let token = sessionStorage.getItem("token")
+//let payload = parseJwt(token)
+//console.log(payload)
+//console.log(payload.id_usuario)
+//var element = document.getElementById('idUsuario')
+//element.innerHTML = payload.id_usuario
+//};
+
+function logaradmin() {
+
+	console.log("entrou")
+	let url = "http://localhost:8080/api/administrador/login";
+	let senha = document.getElementById("senha").value;
+	let nif = document.getElementById("nif").value;
+
+	console.log(senha)
+	console.log(nif)
+	console.log(url)
+
+	var admin = {
+		nif,
+		senha
+	}
+
+	console.log(admin)
+
+	const myHeaders = new Headers()
+	myHeaders.append("Content-Type", "application/json")
+
+	let fetchData = {
+		method: 'POST',
+		body: JSON.stringify(admin),
+		headers: myHeaders
+	}
+
+	console.log('passou aqui')
+
+	fetch(url, fetchData).then((resp) => {
+		console.log("AAAAAAAAAAAAAAAAAAAAAAAA")
+		console.log(resp)
+		resp.json().then((resposta) => {
+			console.log(resposta)
+
+			// tentou deu certo
+			sessionStorage.setItem("token", resposta.token)
+			console.log(resposta.token)
+
+			// console.log(sessionStorage)
+
+			window.location.href = '/painelReserva'
+		})
+
+			.catch((e) => {
+				// le o erro
+				console.log(e)
+			})
+	})
+
+	//location.reload();
+	// fazPost(url, body)
+};
+
+
+function parseJwt(token) {
+	var base64Url = token.split('.')[1];
+	var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+		return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+	}).join(''));
+
+	return JSON.parse(jsonPayload);
+};
+colocaValor()
+//logarUsuario()
